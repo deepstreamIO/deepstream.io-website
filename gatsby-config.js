cb83063b9327fe87a5f0b211a9279366f946425a
Yasser Fadl <yasser.fadl@gmail.com>
2019-08-02 11:55:31 +0200

    Adding google tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -123,5 +123,16 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-63583386-1",
+        head: false,
+        anonymize: true,
+        exclude: [],
+        pageTransitionDelay: 0,
+        cookieDomain: "deepstream.io",
+      },
+    }
   ],
 }
