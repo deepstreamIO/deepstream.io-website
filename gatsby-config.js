879110690e821c888c1ab1483574819eb06bc32d
Ananda Umamil <zweimach@zweimach.org>
2020-06-08 19:00:29 +0700

    Use gatsby-plugin-manifest for favicon & metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `deepstream.io`,
+    description: `A realtime server for the modern web`,
+    author: `deepstreamHub GmbH`,
   },
   plugins: [
     `gatsby-transformer-sharp`,
@@ -108,18 +108,18 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `deepstream.io`,
+        short_name: `deepstream.io`,
+        start_url: `/`,
+        background_color: `#FFFFFF`,
+        theme_color: `#0FBBEC`,
+        display: `minimal-ui`,
+        icon: `static/images/deepstream-elton-logo-startpage.svg`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
