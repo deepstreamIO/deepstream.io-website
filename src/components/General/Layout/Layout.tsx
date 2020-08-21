a689481ada0f8e0ed34e1c2bf6308151232849b2
jaime-ez <jechegarayz@gmail.com>
2020-08-20 21:32:29 -0400

    [fix] hide banner and github url

--- src/components/General/Layout/Layout.tsx
+++ src/components/General/Layout/Layout.tsx
@@ -15,31 +15,32 @@ export const Layout: React.FunctionComponent<LayoutProps> = ({ children, pageCla
     return <div className={pageClass}>
         <SEO />
         <Header/>
-{/*        <div style={{
+        <div style={{
             boxSizing: 'border-box',
-            paddingTop: "10px",
+            // paddingTop: "10px",
             paddingBottom: "10px",
-            height: "60px",
+            // height: "60px",
             textAlign: 'center',
             width: '100%',
-            background: `repeating-linear-gradient(
-                45deg,
-                #606dbc,
-                #0FBBEC 10px,
-                #465298 10px,
-                #0FBBEC 20px
-              )`
+            // background: `repeating-linear-gradient(
+            //     45deg,
+            //     #606dbc,
+            //     #0FBBEC 10px,
+            //     #465298 10px,
+            //     #0FBBEC 20px
+            //   )`
+            background: `#0FBBEC`
         }}>
-            <div style={{ paddingTop: '10px' }}>
+            {/*<div style={{ paddingTop: '10px' }}>
                 <Link style={{
                     fontSize: '20px',
                     color: 'yellow',
                     fontWeight: 500,
                     paddingTop: '20px'
                 }} to="/blog/20200519-deepstream-maintenance-mode/">Deepstream is in maintenance mode, contributors needed</Link>
-            </div>
+            </div>*/}
         </div>
-*/}        <div>
+        <div>
             {children}
         </div>
         <Footer hasSideBar={hasSideBar} />
--- src/components/General/Navigation/Navigation.tsx
+++ src/components/General/Navigation/Navigation.tsx
@@ -4,7 +4,7 @@ import * as React from "react"
 export const Navigation = ({ mobile = false }) => (
     <ul className={`links ${mobile ? 'mobile-menu' : 'desktop-menu'}`}>
         <li className="navigation-link">
-            <a target="_blank" href="https://github.com/deepstreamIO/deepstream.io">
+            <a target="_blank" href="https://github.com/deepstreamIO">
                 github
             </a>
         </li>
